38cc08c491d6f6634d07c2c3c9f8776eccbed511
Jacek Kałużny <jacek@duszekjk.pl>
2018-11-26 13:40:14 +0100

    Initial Commit

---
+++ projektZMO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  projektZMO
+//
+//  Created by Jacek Kaluzny on 26/11/2018.
+//  Copyright © 2018 duszekjk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
